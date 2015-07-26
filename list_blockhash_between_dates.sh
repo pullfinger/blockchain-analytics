3f80032726439adbf4cd875485ab5d6e76319727
Cole Albon <cole@marvelcomputing.com>
2015-07-25 23:06:21 -0700

    add list_txid_by_blockhash.js

--- list_blockhash_between_dates.sh
+++ list_blockhash_between_dates.sh
@@ -1,4 +1,4 @@
-# usage: ./list_blockhash_between_dates.sh "2015-03-01 00:00:00 -0800" "2015-03-02 00:00:00 -0800"
+# usage: ./list_blockhash_between_dates.sh "2015-03-01 00:00:00 -0700" "2015-03-02 00:00:00 -0700"
 start_date=$1
 end_date=$2
 block_height_low=`echo $start_date | ./get_blockheight_by_date.sh`
---
+++ src/list_txid_by_blockhash.js
@@ -0,0 +1,19 @@
+//usage: date -v-1m +%s | node src/blockcount_by_date.js
+//        block height exactly one month ago
+//  date -j -f "%Y-%m-%d" "2010-10-02" "+%s" | node src/blockcount_by_date.js
+//        any date converted to unix time
+
+var readline = require('readline')
+var bulkchain = require('../lib/bulkchain.js')
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', function(line){
+    list_txid_by_blockhash(line, function(txidlist) {
+        setTimeout(console.log(JSON.stringify(txidlist)), 0)
+    })
+})
