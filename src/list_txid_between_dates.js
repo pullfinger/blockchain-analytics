2740e0837b64d2c134ba32c968f0e2ebb9b0e9f8
Cole Albon <cole@marvelcomputing.com>
2015-07-26 22:53:53 -0700

    beginning removal of cult cargo

--- src/list_txid_between_dates.js
+++ src/list_txid_between_dates.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-//usage: node src/list_blockhash_between_dates --startdate=`date -j -f %Y%m%d%H%M%S 20150701000000 +%s` --enddate=`date -j -f %Y%m%d%H%M%S 20150702000000 +%s`
+//usage: node src/list_txid_between_dates --startdate=`date -j -f %Y%m%d%H%M%S 20150701000000 +%s` --enddate=`date -j -f %Y%m%d%H%M%S 20150702000000 +%s`
 // 2 months ago to 1 month ago
 // july 1 2015 local time
 // date -j -f %Y%m%d%H%M%S 20150701000000
